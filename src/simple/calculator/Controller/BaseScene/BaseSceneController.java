6b55b12a77d5df555ffa4dd43019c647e74beac6
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-09 13:58:47 +0600

    Addition completed

--- src/simple/calculator/Controller/BaseScene/BaseSceneController.java
+++ src/simple/calculator/Controller/BaseScene/BaseSceneController.java
@@ -6,9 +6,10 @@ package simple.calculator.Controller.BaseScene;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -21,10 +22,19 @@ public class BaseSceneController implements Initializable {
      * Initializes the controller class.
      */
     @FXML
-    private Label label;
+    private TextField Number1;
     @FXML
-    private void buttonHanldeler(){
-        System.out.println("Hellow world");
+    private TextField Number2;
+    @FXML
+    private TextField Result;
+    @FXML
+    private void buttonHanldeler(ActionEvent Event){
+        String n1 = Number1.getText();
+        String n2 = Number2.getText();
+        double d1 = Double.parseDouble(n1);
+        double d2 = Double.parseDouble(n2);
+        double result = d1+d2;
+        Result.setText(""+result);
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
